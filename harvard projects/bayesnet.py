759b911632e9ab289e2a38e185d63421bbb83b2e
goodboi1234 <142526213+goodboi1234@users.noreply.github.com>
2023-12-12 09:01:29 +0530

    Update bayesnet.py

--- harvard projects/bayesnet.py
+++ harvard projects/bayesnet.py
@@ -1,60 +1,71 @@
-import pomegranate as p
-#MAKING THE RAIN PROBS WHY DISCREATE DISTRIBUTION ? BECAUSE IT'S THE PARENT CLASS.
-rain = p.Node(p.DiscreteDistribution({
-    "none":0.7,
-    "light":0.2,
-    "heavy":0.1
-}), name = "rain")
+from pomegranate import  bayesian_network
+from pomegranate import Node
 
-#NODE FOR TRACk MAINTAINANCE HERE WE TYPED CONDITIONAL PROBABLITY TO REPRESENT THAT THE PROBABLITY
-#OF MAINTAINANCE WILL DEPEND ON THE PROBABLITY OF RAIN FOR THAT WE WILL WRITE CONDITIONALPROBABLITY DISTRIBUTION
-#SO, THERE ARE ACTUALL ONE ARGUMENT FOR THE CONDITIONALPROBABLITY DISTRIBUTION AND THATS THE VALUE OF 
-#THE PROBABLITIES IN THE FORMAT OF [EVENT OFTHE CONDITION,]
-#the arguments of node is the condutionalprobablity , rain distribution and the last is the name of the node 
-maintainance = p.Node(p.ConditionalProbablityDistribution(
-    ["none" , "yes" , 0.4],
-    ["none" , "no" , 0.6],
-    ["light" , "no" , 0.8],
-    ["heavy" , "yes" , 0.1],
-    ["light" , "yes" , 0.2],
-    ["heavy" , "yes" , 0.9]
-    , [rain.distribution]
-)  , name = "maintainance")
+from pomegranate import DiscreteDistribution
+from pomegranate import ConditionalProbabilityDistribution
+from pomegranate import ConditionalProbabilityTable
+import json
+import os
+import pandas as pd 
+import numpy as np 
+import re
+import torch
 
-#THE CONDITIONAL PROBABLITY OF TRAIN
-train = p.Node(p.ConditionalProbablityTable(
-   [ ["none" , "yes" , "on time" , 0.8],
-    ["none" , "yes" , "delayed" , 0.2],
-    ["none" , "no" , "on time" , 0.9],
-    ["none" , "yes" , "delayed" , 0.1],
-    ["light" , "no" , "delayed" , 0.3],
-    ["light" , "no" ,"on time" , 0.7 ],
-    ["light" , "yes" , "delayed" , 0.4],
-    ["light" , "yes" , "on time" , 0.6],
-    ["heavy" , "yes" , "on time" , 0.4],
-    ["heavy" , "yes" , "delayed" , 0.6],
-    ["heavy" , "no" , "on time" , 0.5],
-    ["heavy" , "no" , "delayed" , 0.5],]
-,[rain.distribution , maintainance.distribution] ),  name = "train")
+# Making the Rain probabilities. Using DiscreteDistribution because it's the parent class.
+rain1 = DiscreteDistribution({
+    "none": 0.7,
+    "light": 0.2,
+    "heavy": 0.1
+})
 
+#MAKING THE NODE OUT OF IT
+rain = Node(rain1 , name = "rain")
 
-#CLEARING WITH THE APPOINTMENT MODULE
-appointment = p.Node(p.ConditionalProbablityTable([
-    ["on time" , "attend" , 0.9],
-    ["on time" , "missed" , 0.1],
-    ["delayed" , "attend" , 0.6],
-    ["delayed" , "missed" , 0.4]
-], [train.distribution]), name = "appointment")
+# Node for Track Maintenance. Typed conditional probability to represent that the probability
+# of maintenance will depend on the probability of rain. Using ConditionalProbabilityDistribution.
+maintainance = Node(ConditionalProbabilityDistribution(
+    ["none", "yes", 0.4],
+    ["none", "no", 0.6],
+    ["light", "no", 0.8],
+    ["light", "yes", 0.2],
+    ["heavy", "yes", 0.9],
+    ["heavy", "no", 0.1]
+), name="maintainance")
 
-#NOW CLEARING WITH THE BAYSINA NETWORK
-model = p.BaysianNetwork()
-model.add_states(rain , maintainance , train , appointment)
+# The conditional probability of Train.
+train = Node(ConditionalProbabilityTable(
+    [["none", "yes", "on time", 0.8],
+     ["none", "yes", "delayed", 0.2],
+     ["none", "no", "on time", 0.9],
+     ["none", "no", "delayed", 0.1],
+     ["light", "no", "delayed", 0.3],
+     ["light", "no", "on time", 0.7],
+     ["light", "yes", "delayed", 0.4],
+     ["light", "yes", "on time", 0.6],
+     ["heavy", "yes", "on time", 0.4],
+     ["heavy", "yes", "delayed", 0.6],
+     ["heavy", "no", "on time", 0.5],
+     ["heavy", "no", "delayed", 0.5]
+ ], [rain.distribution, maintainance.distribution]), name="train")
 
-#NOW WE HAVE TO CONNECT NODES TO TELL AI
-model.add_edge(rain , maintainance)
-model.add_edge(rain , train)
+# Clearing with the Appointment module.
+appointment = Node(ConditionalProbabilityTable([
+    ["on time", "attend", 0.9],
+    ["on time", "missed", 0.1],
+    ["delayed", "attend", 0.6],
+    ["delayed", "missed", 0.4]
+], [train.distribution]), name="appointment")
+
+# Now clearing with the Bayesian network.
+model = p.BayesianNetwork()
+model.add_states(rain, maintainance, train, appointment)
+
+# Connecting nodes to define dependencies.
+model.add_edge(rain, maintainance)
+model.add_edge(rain, train)
 model.add_edge(maintainance, train)
-model.add_edge(train , appointment)
+model.add_edge(train, appointment)
 
-#FINALIZE MODEL
+# Finalize the model.
 model.bake()
+
